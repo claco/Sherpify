7ae4da97628ee00e310fce2a17071f13a238eadd
Christopher H. Laco <claco@chrislaco.com>
2011-03-29 21:54:55 -0400

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sherpify::Application.config.secret_token = '82afeb16ccae052bfcbaa73ddfea30fe03868f10fe363e0ad283bf611ed7ad0d085d0ff089830bb9ca3f31a39178a62407a7651ff0363b1d8e5e6e129a1ead8d'
